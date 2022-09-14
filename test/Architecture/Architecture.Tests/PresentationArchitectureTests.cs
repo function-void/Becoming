fdf542b33f3a6c8494910b5c97f075c3cfb42d2b
Amirbek_Akhmetov <lieber_kz@mail.ru>
2022-09-14 10:20:07 +0600

    feat add presentation test

--- test/Architecture/Architecture.Tests/PresentationArchitectureTests.cs
+++ test/Architecture/Architecture.Tests/PresentationArchitectureTests.cs
@@ -1,27 +1,62 @@
-﻿using FluentAssertions;
+﻿using Becoming.Core.Common.Presentation;
+using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyModel;
+using NetArchTest.Rules;
 using System.Reflection;
 
 namespace Architecture.Tests;
 
 public sealed class PresentationArchitectureTests
 {
+    private const string BUILDS_NAME = "Becoming";
+
     [Fact]
     public void Controllers_Should_Be_Inheritanced_By_With_ApiController()
     {
         // Arrange
-        var presentationAssemblyList = DependencyContext.Default.RuntimeLibraries
-            .Where(x => x.Name.Contains("Becoming"))
+        List<Assembly> presentationAssemblyList = DependencyContext.Default.RuntimeLibraries
+            .Where(x => x.Name.Contains(BUILDS_NAME))
             .Select(x => Assembly.Load(x.Name))
             .Where(assembly => assembly.DefinedTypes.Where(t => !t.IsAbstract)
                 .Any(t => t.BaseType == typeof(ControllerBase)))
             .ToList();
 
         // Act
-        var result = presentationAssemblyList.Count;
 
         // Assert
-        Assert.Equal(0, result);
+        presentationAssemblyList.Should().BeOfType<List<Assembly>>();
+        presentationAssemblyList.Should().NotBeNull().And.HaveCount(0);
+    }
+
+    [Fact]
+    public void Endpoints_Which_Post_Should_Be_Async()
+    {
+        // Arrange
+        var presentationAssemblyList = DependencyContext.Default.RuntimeLibraries
+            .Where(x => x.Name.Contains(BUILDS_NAME))
+            .Select(x => Assembly.Load(x.Name))
+            .Where(assembly => assembly.DefinedTypes.Where(t => !t.IsAbstract)
+                .Any(t => t.BaseType == typeof(ApiController)))
+            .ToList();
+
+        var apiControllerTypes = Types.InAssemblies(presentationAssemblyList)
+            .GetTypes()
+            .ThatAreClasses()
+            .ThatAreDecoratedWithOrInherit<ApiControllerAttribute>()
+            .ToList();
+
+        // Act
+
+        // Assert
+        foreach (var controllerType in apiControllerTypes)
+        {
+            controllerType.Methods()
+                .ThatReturn<ActionResult>()
+                .ThatReturn<IActionResult>()
+                .ThatAreDecoratedWith<HttpPostAttribute>()
+                .Should()
+                .BeAsync();
+        }
     }
 }
