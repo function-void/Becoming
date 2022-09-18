a33abaca01afd9ee9b510f251e5d9d2a96db3806
Amirbek Akhmetov <lieber_kz@mail.ru>
2022-09-18 22:02:56 +0600

    add specification

---
+++ src/Core/Becoming.Core.Common/Becoming.Core.Common.Primitives/Models/ExpressionSpecification.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+
+namespace Becoming.Core.Common.Primitives.Models;
+
+public abstract class ExpressionSpecification<T> : ISpecification<T>
+{
+    private Func<T, bool> _expressionFunc;
+    private Func<T, bool> ExpressionFunc => _expressionFunc ??= Expression.Compile();
+
+    public Expression<Func<T, bool>> Expression { get; }
+
+    protected ExpressionSpecification(Expression<Func<T, bool>> expression)
+    {
+        Expression = expression;
+    }
+
+    public bool IsSatisfied(T obj)
+    {
+        var result = ExpressionFunc(obj);
+        return result;
+    }
+}
---
+++ src/Core/Becoming.Core.Common/Becoming.Core.Common.Primitives/Models/ISpecification.cs
@@ -0,0 +1,6 @@
+﻿namespace Becoming.Core.Common.Primitives.Models;
+
+public interface ISpecification<in T>
+{
+    bool IsSatisfied(T obj);
+}
