be5135016b6e64e87795f97e8da8e3812093085d
Amirbek Akhmetov <lieber_kz@mail.ru>
2022-09-25 17:49:20 +0600

    fix

--- src/Core/Becoming.Core.TaskManager/Becoming.Core.TaskManager.Application/Commands/CreateTaskManager/CreateTaskManagerRequest.cs
+++ src/Core/Becoming.Core.TaskManager/Becoming.Core.TaskManager.Application/Commands/CreateTaskManager/CreateTaskManagerRequest.cs
@@ -4,7 +4,7 @@ using Becoming.Core.Common.Abstractions.Interfaces;
 
 namespace Becoming.Core.TaskManager.Application.Commands.CreateTaskManager;
 
-public sealed class CreateTaskManagerRequest : IDtoObject<TaskManagerAggregate>
+public sealed record class CreateTaskManagerRequest : IDtoObject<TaskManagerAggregate>
 {
     public string Title { get; set; } = null!;
     public string Category { get; set; } = null!;
