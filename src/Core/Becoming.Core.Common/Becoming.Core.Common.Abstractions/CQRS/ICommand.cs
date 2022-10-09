f7d4dff85591f1840e469b71845fa4887fb4f04f
Amirbek Akhmetov <lieber_kz@mail.ru>
2022-10-10 01:05:29 +0600

    feat

--- src/Core/Becoming.Core.Common/Becoming.Core.Common.Abstractions/CQRS/ICommand.cs
+++ src/Core/Becoming.Core.Common/Becoming.Core.Common.Abstractions/CQRS/ICommand.cs
@@ -8,3 +8,5 @@ public interface ICommand<out TResponse> : IRequest<TResponse> { }
 
 public abstract record class CommandWithDto<TRequest, TResponse>(TRequest Dto) : ICommand<TResponse>
     where TRequest : IDtoObject<Entity>;
+
+//TODO: add functionality after used creat command, and call up event aggregate send notification
